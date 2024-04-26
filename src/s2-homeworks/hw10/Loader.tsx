ac47c9a9c0303f8953f50c861849f265c2ebacd1
AlenaZinovieva <sas1493@mail.ru>
2024-04-26 11:34:02 +0300

    updates

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,4 @@
 import s from './Loader.module.css'
+import preloader from './../../assets/images/loading.svg'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <img src={preloader} className={s.loader}/>
