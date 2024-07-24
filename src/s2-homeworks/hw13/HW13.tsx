e84d6bb9eed1d3fe94f1d951c42a59213d0d66db
AlenaZinovieva <sas1493@mail.ru>
2024-07-24 14:45:22 +0300

    updates

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -36,12 +36,29 @@ const HW13 = () => {
             .then((res) => {
                 setCode('Код 200!')
                 setImage(success200)
-                // дописать
+                setText('...всё ок\n код 200 - обычно означает что скорее всего всё ок')
+                setInfo('')
 
             })
             .catch((e) => {
-                // дописать
-
+                if(x === false) {
+                    setCode('Ошибка 500!')
+                    setImage(error500)
+                    setText('эмитация ошибки на сервере ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных')
+                    setInfo('')
+                }
+                if(x === undefined) {
+                    setCode('Ошибка 400!')
+                    setImage(error400)
+                    setText('Ты не отправил success в body вообще! ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!')
+                    setInfo('')
+                }
+                if(x === null) {
+                    setCode('Error')
+                    setImage(errorUnknown)
+                    setText('Network Error AxiosError')
+                    setInfo('')
+                }
             })
     }
 
@@ -55,7 +72,7 @@ const HW13 = () => {
                         id={'hw13-send-true'}
                         onClick={send(true)}
                         xType={'secondary'}
-                        // дописать
+                        disabled={info !== ''}
 
                     >
                         Send true
@@ -64,7 +81,7 @@ const HW13 = () => {
                         id={'hw13-send-false'}
                         onClick={send(false)}
                         xType={'secondary'}
-                        // дописать
+                        disabled={info !== ''}
 
                     >
                         Send false
@@ -73,7 +90,7 @@ const HW13 = () => {
                         id={'hw13-send-undefined'}
                         onClick={send(undefined)}
                         xType={'secondary'}
-                        // дописать
+                        disabled={info !== ''}
 
                     >
                         Send undefined
@@ -82,7 +99,7 @@ const HW13 = () => {
                         id={'hw13-send-null'}
                         onClick={send(null)} // имитация запроса на не корректный адрес
                         xType={'secondary'}
-                        // дописать
+                        disabled={info !== ''}
 
                     >
                         Send null
